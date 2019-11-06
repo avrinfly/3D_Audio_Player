8bc8fe91a950e4d4c0051c8fbc897230e5eb29df
avrin.live.cn@outlook.com <avrin.live.cn@outlook.com>
2019-11-06 22:04:59 +0800

    add config file and perfect DOM structure

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+<!--
+ * @Description: 
+ * @Author: hetengfei
+ * @Github: https://github.com/avrinfly
+ * @Date: 2019-11-06 22:00:48
+ * @LastEditors: hetengfei
+ * @LastEditTime: 2019-11-06 22:01:52
+ -->
 # 3D Audio player
 3D音乐播放器
 
@@ -5,3 +13,7 @@
 > webGL
 
 a 3d audio player built with three.js
+
+## 步骤：
+
+- config配置文件
---
+++ js/index.js
@@ -0,0 +1,20 @@
+/*
+ * @Description: 
+ * @Author: hetengfei
+ * @Github: https://github.com/avrinfly
+ * @Date: 2019-11-06 21:55:14
+ * @LastEditors: hetengfei
+ * @LastEditTime: 2019-11-06 22:00:59
+ */
+/*
+ * 3D音乐播放器
+ * revision 0.0.1
+ * Nov 5,2019 Avrinfly
+*/
+let Config = function() {
+  this.appName = 'HTML 3D Audio Spectrum Visualizer'; // 播放信息
+  this.url = ''; // 音乐路径
+  this.playInfo = document.getElementById('playInfo'); //播放信息
+  this.playerMainBody = document.getElementById('playerMainBody'); //播放器主体
+}
+
