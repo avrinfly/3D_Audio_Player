463b34f468a2f37323da53e91e4892f1adf3ddbc
avrin.live.cn@outlook.com <avrin.live.cn@outlook.com>
2019-11-06 22:11:42 +0800

    add init function

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
  * @Github: https://github.com/avrinfly
  * @Date: 2019-11-06 22:00:48
  * @LastEditors: hetengfei
- * @LastEditTime: 2019-11-06 22:01:52
+ * @LastEditTime: 2019-11-06 22:10:25
  -->
 # 3D Audio player
 3D音乐播放器
@@ -17,3 +17,4 @@ a 3d audio player built with three.js
 ## 步骤：
 
 - config配置文件
+- 初始化 init函数 初始化内容包括 AudioContext、cancelAnimationFrame、requestAnimationFrame三个window对象自带的和音频处理相关的API，都是HTML5新增的
--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
  * @Github: https://github.com/avrinfly
  * @Date: 2019-11-06 21:55:14
  * @LastEditors: hetengfei
- * @LastEditTime: 2019-11-06 22:00:59
+ * @LastEditTime: 2019-11-06 22:11:06
  */
 /*
  * 3D音乐播放器
@@ -18,3 +18,12 @@ let Config = function() {
   this.playerMainBody = document.getElementById('playerMainBody'); //播放器主体
 }
 
+Config.prototype = {
+    //初始化操作 准备音频和场景
+    init() {
+        window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
+        // 只有Firefox支持mozAnimationStartTime属性,其他浏览器可以使用Date.now()来替代
+        window.cancelAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
+        window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext
+    },
+}
