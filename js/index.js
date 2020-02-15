52017c47c37eb3414d8e4b48f8ecc56c2637cdbd
avrin.live.cn@outlook.com <avrin.live.cn@outlook.com>
2020-02-15 23:56:20 +0800

    content amendments

--- js/index.js
+++ js/index.js
@@ -19,34 +19,34 @@ let Config = function() {
 }
 
 Config.prototype = {
-    //初始化操作 准备音频和场景
-    init() {
-        window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
-        // 只有Firefox支持mozAnimationStartTime属性,其他浏览器可以使用Date.now()来替代
-        window.cancelAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
-        window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext
+  //初始化操作 准备音频和场景
+  init() {
+    window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
+    // 只有Firefox支持mozAnimationStartTime属性,其他浏览器可以使用Date.now()来替代
+    window.cancelAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
+    window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext
+    // 准备音频
+    //准备场景
+    this.__initScene__();
+  },
 
-        //准备场景
-        this.__initScene__();
-    },
+  __initScene__() {
+    let __that__ = this;
+    WIDTH = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    HEIGHT = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    DISTANCE = 1; //柱子间的间隔（米）
+    PWIDTH = 2; //每个柱子的宽度 
 
-    __initScene__() {
-        let __that__ = this;
-        WIDTH = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        HEIGHT = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-        DISTANCE = 1; //每个柱子的间隔
-        PWIDTH = 2; //每个柱子的宽度
-
-        scene = new THREE.Scene(); //创建场景
-        camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000); //创建相机
-        scene.add(camera); //将相机添加到场景中
-        render = new THREE.WebGLRenderer(); //创建渲染器
-        render.setSize(WIDTH, HEIGHT); // 设置渲染器的尺寸
-        __that__.playerMainBody.appendChild(render.domElement); //将渲染器添加到播放器主体里
-    }
+    scene = new THREE.Scene(); //创建场景
+    camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000); //创建相机
+    scene.add(camera); //将相机添加到场景中
+    render = new THREE.WebGLRenderer();// 创建渲染器
+    render.setSize(WIDTH, HEIGHT); // 设置渲染器的尺寸
+    __that__.playerMainBody.appendChild(render.domElement); //将渲染器添加到播放器主体里
+  }
 }
 
 window.onload = function () {
-    let config = new Config();
-    config.init();
+  let config = new Config();
+  config.init();
 }
