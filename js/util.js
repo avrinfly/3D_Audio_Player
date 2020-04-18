75c441b754444daf37ae18bc3f94805a1ac28dc5
avrin.live.cn@outlook.com <avrin.live.cn@outlook.com>
2020-04-19 02:19:41 +0800

    util.js added

---
+++ js/util.js
@@ -0,0 +1,27 @@
+(() => {
+  let defining = {
+    // 判断音频格式类型
+    judgeAudioFormat: function (type) {
+      // /text+/.test(file.type) 判断是否是text格式
+      const typeList = {
+        "": false,
+        'audio/flac': true,
+        'audio/mp3': true,
+        'text/plain': false
+      }
+      return typeList[type]
+    },
+    queryFileName(url) {
+      const regx = '[^/]+(?!.*/)';
+      let target = url.match(regx)[0]
+      return target
+    },
+    c: function () {
+      console.log('我是c');
+    }
+};
+
+Object.keys(defining).forEach(key => {
+  window[key] = defining[key];
+  });
+})(); 
